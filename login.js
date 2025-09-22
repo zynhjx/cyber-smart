1c66f8ff11a3aa3f6de8799567247284a9f00228
lynxairu <lynxairu.ichiro@gmail.com>
2025-09-22 12:15:01 +0800

    add register and login functions

---
+++ login.js
@@ -0,0 +1,22 @@
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const res = await axios.post(
+            "https://cyber-smartbackend-production-ffc3.up.railway.app/login",
+            { username, password }
+        );
+
+        console.log("Response:", res.data);
+
+        if (res.data.success) {
+            window.location.href = "/dashboard.html";
+        }
+    } catch (err) {
+        console.error("Error:", err.response?.data || err.message);
+    }
+});
---
+++ register.js
@@ -0,0 +1,33 @@
+
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    try {
+    const res = await axios.post(
+        "https://cyber-smartbackend-production-ffc3.up.railway.app/register",
+        { username, email, password }
+    );
+
+    console.log("Response:", res.data);
+
+    if (res.data.success) {
+        alert("Account created successfully!");
+        window.location.href = "login.html";
+    } else {
+        alert(res.data.message || "Registration failed.");
+    }
+    } catch (err) {
+        console.error("Error:", err.response?.data || err.message);
+        alert("Something went wrong. Try again.");
+    }
+});
