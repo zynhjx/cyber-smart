176dd6c39d77669dcafc179fa291d338b1fa82ef
lynxairu <lynxairu.ichiro@gmail.com>
2025-09-22 10:43:12 +0800

    new start

--- index.js
+++ index.js
@@ -14,48 +14,18 @@ infi_castle.preload = "auto";
 const minecraft_mp3 = new Audio("./minecraft_click.mp3")
 minecraft_mp3.preload = "auto";
 
-
-
-function btns_visibility() {
-    container.style.display = "none";
-    btns.style.display = "flex";
-    back_btn.style.display = "block";
-}
-
-function welcome_page_visibility() {
-    container.style.display = "flex";
-    btns.style.display = "none";
-    back_btn.style.display = "none";
-}
-
-
 video.addEventListener("canplaythrough", function() {
     preloader.style.display = "none"; 
 });
 
-if (sessionStorage.getItem("clicked") === "true") {
-    btns_visibility();
-}
+
 
 btn.addEventListener("click", function() {
-    btns_visibility();
+    if () {
 
-    sessionStorage.setItem("clicked", "true")
-
+    }
 })
 
-back_btn.addEventListener("click", function() {
-    welcome_page_visibility();
-
-    sessionStorage.setItem("clicked", "false")
-})
-
-btns_fe.forEach(btn => {
-    btn.addEventListener("mouseenter", function() {
-        const audio = minecraft_mp3.cloneNode();
-        audio.play();
-    })
-})
 
 
 
